c635682bbda3f4e2b8a7fed9ec1401f6d0782bbb
Marcelo <mvinicius.louza@gmail.com>
2022-01-11 09:36:49 -0300

    Adicionar arquivos de projeto.

---
+++ Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Static_Class
+{
+    internal class Calculator
+    {
+        public static double Pi = 3.14;
+
+        public static double Circunference(double r)
+        {
+            return 2 * Pi * r;
+        }
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Static_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter valeu of ray: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("Circunference: {0}", Calculator.Circunference(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: {0}", Calculator.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PI value: {0}", Calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
